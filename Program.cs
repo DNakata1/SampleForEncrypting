e41a702a2c268fea91db3e107064d94cf8173ca0
DNakata1 <75272071+DNakata1@users.noreply.github.com>
2021-04-19 09:53:22 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Crypting
+{
+    class Program
+    {
+        class Encrypting
+        {
+            public char RandomNum(char random)
+            {
+                Random rand = new Random();
+                int rnd = rand.Next(-7, 7);
+                char result;
+                return result = (char)(random + rnd);
+            }
+            public void EncryptFun(String Str)
+            {
+                char[] charOf = Str.ToCharArray();
+                char EmptyChar;
+                List<char> CharOutput = new List<char>();
+                foreach (char x in charOf)
+                {
+                    EmptyChar = RandomNum(x);
+                    CharOutput.Add(EmptyChar);
+                }
+                Console.WriteLine("Encrypted: "+String.Join("", CharOutput));
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the string here:");
+            String input = Console.ReadLine();
+            Encrypting En = new Encrypting();
+            En.EncryptFun(input);
+            Console.Read();
+        }
+    }
+}
